02eeaaf3b6874d36f90d16c5292eb982e3b04c4c
czh <chen.zhihan@haojun-info.com>
2021-12-30 17:28:56 +0800

    czh

--- src/utils/cellClass.ts
+++ src/utils/cellClass.ts
@@ -1,10 +1,43 @@
 /*
  * @Date: 2021-12-30 16:17:43
  * @LastEditors: CZH
- * @LastEditTime: 2021-12-30 16:29:21
+ * @LastEditTime: 2021-12-30 17:23:29
  * @FilePath: /configforpagedemo/src/utils/cellClass.ts
  */
+
+
+
+/**
+ * @name: templateCellOptions_elementType
+ * @description: 适用于infoCellTemplate的特殊处理字段类型
+ * @authors: CZH
+ * @Date: 2021-12-30 17:18:49
+ */
+export enum templateCellOptions_elementType {
+    tags,
+    keyWords,
+    button,
+}
+/**
+ * @name: templateCellOptions
+ * @description: 特殊处理方案列表
+ * @authors: CZH
+ * @Date: 2021-12-30 17:20:58
+ */
+export interface templateCellOptions {
+    elemetType: templateCellOptions_elementType,
+    return: Function,
+}
+
+/**
+ * @name: cellMakerOptions
+ * @description: 构建单元
+ * @authors: CZH
+ * @Date: 2021-12-30 17:21:14
+ */
 export interface cellMakerOptions {
-    style: string,
-    width: string,
+    style: String,
+    width: String,
+    needTranslate: Boolean,
+    translateFunc: Function,
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,33 @@
 /*
  * @Date: 2021-12-30 15:11:39
  * @LastEditors: CZH
- * @LastEditTime: 2021-12-30 16:29:35
+ * @LastEditTime: 2021-12-30 17:25:34
  * @FilePath: /configforpagedemo/src/utils/index.ts
  */
 import { cellMakerOptions } from "./cellClass";
+import { templateCellOptions } from "./cellClass";
 
 
 
 export default {
     /**
      * @name: tableCellMaker
-     * @description: 构建表格单元
+     * @description: 构建表格单元 - @TODO 也可以兼容使用到输入&显示单元
      * @authors: CZH
      * @Date: 2021-12-30 15:18:29
      */
-    tableCellMaker: (prop: string, label: string, template: Array<Object>, options: cellMakerOptions) => {
+    tableCellMaker: (prop: string, label: string, template: Array<templateCellOptions>, options: cellMakerOptions) => {
         return {
+            key:prop,
+            value:prop,
             prop,
             label,
             template,
             ...options
         }
     },
+
+    
+
+    
 }
