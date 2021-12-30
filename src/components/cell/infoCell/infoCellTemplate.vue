e49240fb2aa8dd555123ed4668c70324c7b17e63
czh <chen.zhihan@haojun-info.com>
2021-12-30 09:37:07 +0800

    czh

--- src/components/cell/infoCell/infoCellTemplate.vue
+++
@@ -1,7 +0,0 @@
-<!--
- * @Date: 2021-12-28 14:56:58
- * @LastEditors: CZH
- * @LastEditTime: 2021-12-28 14:56:58
- * @FilePath: /configforpagedemo/src/components/cell/infoCell/infoCellTemplate.vue
--->
-
