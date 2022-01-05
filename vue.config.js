9f95603cf91f90aa59af0b2c7d03d7c3a7e756a6
czh <chen.zhihan@haojun-info.com>
2022-01-05 10:52:02 +0800

    czh

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+/*
+ * @Date: 2022-01-05 09:21:49
+ * @LastEditors: CZH
+ * @LastEditTime: 2022-01-05 10:51:14
+ * @FilePath: /configforpagedemo/vue.config.js
+ */
+
+
+// development & production
+const Env = process.env.NODE_ENV
+const projectVersion = '0.0.1'
+
+module.exports = {
+    publicPath: '',
+    devServer: {
+        proxy: {}
+    },
+    configureWebpack: {
+        // webpack 配置
+        output: {
+            // 输出重构  打包编译后的 文件名称  【模块名称.环境-版本号.时间戳】
+            filename: `[name].${Env}-${projectVersion}.js`,
+            chunkFilename: `[name].${Env}-${projectVersion}.js`,
+        },
+    }
+}
