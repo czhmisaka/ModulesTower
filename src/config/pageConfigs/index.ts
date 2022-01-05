f8747afb17d9b87f7891ee5abb3d1abc099a8fcb
czh <chen.zhihan@haojun-info.com>
2022-01-05 12:20:33 +0800

    cz

--- src/config/pageConfigs/index.ts
+++ src/config/pageConfigs/index.ts
@@ -1,26 +1,12 @@
 /*
  * @Date: 2021-12-30 19:54:50
  * @LastEditors: CZH
- * @LastEditTime: 2022-01-05 12:10:41
+ * @LastEditTime: 2022-01-05 12:19:28
  * @FilePath: /configforpagedemo/src/config/pageConfigs/index.ts
  */
 
 
-/**
- * @name: requireModule
- * @description: 引用module,目前来看并不能用变量路径来引入文件了，真的难顶啊兄弟们
- * @authors: CZH
- * @Date: 2022-01-05 11:09:09
- */
-const requireModule = require.context(
-    './',
-    true,
-    /\.ts$/
-)
-
-
-let modules: any = {}
-
+// 两个工具函数，看不看都无所谓
 const upperFirst = (str: string) => {
     return str.replace(/^\S/, s => s.toUpperCase())
 }
@@ -30,15 +16,33 @@ const camelCase = function (str: string) {
     })
 }
 
-requireModule.keys().map(fileName => {
-    if (fileName == './index.ts') return;
-    const config_info: any = requireModule(fileName);
-    const config_name: string = upperFirst(
-        camelCase(
-            fileName.split('/').pop().replace(/\.\w+$/, '')
+
+/**
+ * @name: requireModule
+ * @description: 引用module,目前看来只能这么配置路径了兄弟们
+ * @authors: CZH
+ * @Date: 2022-01-05 11:09:09
+ */
+const requireModule = [require.context(
+    './',
+    true,
+    /\.ts$/
+)]
+
+
+let modules: any = {}
+
+requireModule.map(rm => {
+    rm.keys().map(fileName => {
+        if (fileName == './index.ts') return;
+        const config_info: any = rm(fileName);
+        const config_name: string = upperFirst(
+            camelCase(
+                fileName.split('/').pop().replace(/\.\w+$/, '')
+            )
         )
-    )
-    modules[config_name] = config_info
+        modules[config_name] = config_info
+    })
 })
 
 
