97d15bfee8d46ffee6b746e0d9d059a2a3a6ba2b
czhmisaka <czh2233445566@163.com>
2022-06-16 10:35:58 +0800

    czh

---
+++ src/components/basicComponents/grid/module/baseToolComponents/funcComponentLoader.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Date: 2022-06-15 23:34:40
+ * @LastEditors: CZH
+ * @LastEditTime: 2022-06-15 23:34:51
+ * @FilePath: /configforpagedemo/src/components/basicComponents/grid/module/baseToolComponents/funcComponentLoader.vue
+-->
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    componentsDetail: {
+      type: Object,
+      default: () => {
+        return {} as {
+          [key: string]: any;
+        };
+      },
+    },
+  },
+  setup() {},
+});
+</script>
