103ae2b74fd4e2ed5a2461fe34b88d551ba308f4
czhmisaka <czh2233445566@163.com>
2022-10-13 12:36:42 +0800

    czh

--- src/components/basicComponents/grid/module/dataTemplate.ts
+++ src/components/basicComponents/grid/module/dataTemplate.ts
@@ -98,7 +98,7 @@ export interface gridCellTemplate {
     labelNameCN: string,
     key: string,
     gridInfo: componentsGridInfo,
-    gridOrigin: gridOriginTemplate,
+    gridOrigin?: gridOriginTemplate,
     component: cardComponent | any,
     options?: gridCellOptions,
     data?: {
