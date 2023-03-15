038ef9d28d7b2c8bedd45d68422a5b7825d2a97c
czhmisaka <czh2233445566@163.com>
2023-03-15 23:40:22 +0800

    getasyncrouter

--- src/utils/api/admin/routes.ts
+++ src/utils/api/admin/routes.ts
@@ -1,8 +1,8 @@
 /*
  * @Date: 2022-11-04 08:44:53
  * @LastEditors: CZH
- * @LastEditTime: 2023-02-28 19:57:43
- * @FilePath: /configforpagedemo/src/utils/api/admin/routes.ts
+ * @LastEditTime: 2023-03-15 23:39:37
+ * @FilePath: /ConfigForDesktopPage/src/utils/api/admin/routes.ts
  */
 import { useModuleHook } from "@/store/modules/module";
 import { http } from "../../http";
@@ -27,25 +27,7 @@ export const getAsyncRoutes = async (noRefresh: boolean = false) => {
     // });
     // data.filter(Boolean);
     const module = useModuleHook();
-    await module.init([
-      {
-        name: "fuck",
-        path: "fuck",
-        children: [
-          {
-            name: "fuck",
-            path: "fuck",
-            children: [
-              {
-                name: "fuck",
-                path: "fuck",
-                children: [],
-              },
-            ],
-          },
-        ],
-      },
-    ]);
+    await module.init([]);
     return {
       data: module.nowModule.children || [],
     };
